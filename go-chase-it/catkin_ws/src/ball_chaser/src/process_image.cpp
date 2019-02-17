44e8d82a247580f2485a40b2a23d5bd229fb72c9
Sergey Royz <zjor.se@gmail.com>
2019-02-17 10:03:15 -0800

    image processing impl

--- go-chase-it/catkin_ws/src/ball_chaser/src/process_image.cpp
+++ go-chase-it/catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -7,13 +7,53 @@ ros::ServiceClient client;
 
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z) {
-    // TODO: Request a service and pass the velocities to it to drive the robot
+    // Request a service and pass the velocities to it to drive the robot
+    ball_chaser::DriveToTarget req;
+    req.request.linear_x = lin_x;
+    req.request.angular_z = ang_z;
+
+    if (!client.call(req)) {
+        ROS_ERROR("Failed to call service command_robot");
+    }
 }
 
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img) {
+    // ROS_INFO("process_image_callback called; width: %d; height: %d; step: %d", img.width, img.height, img.step);
 
     int white_pixel = 255;
+    int side_width = 2 * img.width / 5;
+
+    int min_x = img.width;
+    int max_x = -1;
+
+    for (int i = 0; i < img.height * img.step; i++) {
+        if (img.data[i] == white_pixel) {
+            int x = (i % img.step) / 3;
+            min_x = x < min_x ? x : min_x;
+            max_x = x > max_x ? x : max_x;
+        }
+    }
+
+    if (max_x != -1) {
+        int ball_x = (min_x + max_x) / 2;
+        ROS_INFO("Whit ball detected at x = %d", ball_x);
+        if (ball_x <= side_width) {
+            // Turn left
+            drive_robot(0.0, 0.1);
+        } else if (ball_x >= img.width - side_width) {
+            // Turn right
+            drive_robot(0.0, -0.1);
+        } else {
+            // Drive forward
+            drive_robot(0.1, 0.0);
+        }
+    } else {
+        // Stop
+        drive_robot(0.0, 0.0);
+    }
+
+    
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
